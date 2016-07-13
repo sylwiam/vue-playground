67ef7a8cfc30c6b32b7afbc1bada8543509e7026
Sylwia Michon <smichon@benchmarkeducation.com>
2016-07-13 14:08:24 -0400

    set up home route and empty home page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,17 +1,6 @@
 <?php
 
 Route::get('/', function() {
-	return view('blog');
+	return view('home');
 });
 
-Route::get('api/messages', array('as' => 'messages-get-all', 'uses' => 'CommentController@getData'));
-
-// get('api/messages', function() {
-// 	return App\Models\Message::all();
-// });
-
-Route::post('api/messages/create', array('as' => 'messages-create', 'uses' => 'CommentController@create'));
-
-Route::get('home-new', function() {
-	return view('home-new');
-});
---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Vue Playground</title>
+        <script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script> 
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <div class="container">
+			<div>
+				<h3>Vue Projects:</h3>
+				<ol class="list-group">
+					<li class="list-group-item">
+					</li>
+					<li class="list-group-item">
+					</li>
+					<li class="list-group-item">
+					</li>
+				</ol>
+			</div>
+		</div>
+    </body>
+</html>
