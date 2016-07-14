8d58e0cb8b116507ee4eb58ca7acb985cba0e763
Sylwia Michon <smichon@benchmarkeducation.com>
2016-07-14 10:13:16 -0400

    add sqllite migration for task table

---
+++ database/migrations/2016_07_13_204428_create_task_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->string('owner')->default('default');
+            $table->boolean('completed')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
